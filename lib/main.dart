d1298e49d06a2f22ed3095a1bdc18c2f0baea950
Sahibzada Arham Anwaar Bugvi <arhamanwaar@gmail.com>
2023-06-05 17:57:26 +0500

    add main page and separate routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_helmet/contact_page.dart';
-import 'package:smart_helmet/login.dart';
-import 'package:smart_helmet/signup.dart';
-import 'package:smart_helmet/start_page.dart';
-import 'package:smart_helmet/welcome.dart';
+import 'package:smart_helmet/routes.dart';
+import 'package:smart_helmet/main_page.dart';
 
-import 'home_page.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: ContactScreen.id,
-      routes: {
-        StartPage.id: (context) => const StartPage(),
-        WelcomePage.id: (context) => const WelcomePage(),
-        SignUpPage.id: (context) => const SignUpPage(),
-        LoginScreen.id: (context) => const LoginScreen(),
-        HomePage.id: (context) => const HomePage(),
-        ContactScreen.id: (context) => const ContactScreen(),
-      },
+      initialRoute: MainPage.id,
+      routes: routes,
       debugShowCheckedModeBanner: false,
     );
   }
